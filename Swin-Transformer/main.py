2103aa7e0c0b0f1a04f69a1cba7a2bb5e2cd7f4b
Jhao Ting Chen <jtchen0528@gmail.com>
2021-07-03 15:55:53 +0800

    update

--- Swin-Transformer/main.py
+++ Swin-Transformer/main.py
@@ -130,7 +130,7 @@ def main(config):
     logger.info("Start training")
     start_time = time.time()
     for epoch in range(config.TRAIN.START_EPOCH, config.TRAIN.EPOCHS):
-        data_loader_train.sampler.set_epoch(epoch)
+        # data_loader_train.sampler.set_epoch(epoch)
 
         train_one_epoch(config, model, criterion, data_loader_train, optimizer, epoch, mixup_fn, lr_scheduler)
         if (epoch % config.SAVE_FREQ == 0 or epoch == (config.TRAIN.EPOCHS - 1)):
