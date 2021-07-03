099ad50cf6e752d0994d5afe5e2276f1144fe6c5
Jhao Ting Chen <jtchen0528@gmail.com>
2021-07-03 16:31:16 +0800

    update

--- Swin-Transformer/main.py
+++ Swin-Transformer/main.py
@@ -157,7 +157,7 @@ def train_one_epoch(config, model, criterion, data_loader, optimizer, epoch, mix
 
     start = time.time()
     end = time.time()
-    for idx, (samples, targets) in enumerate(data_loader)[:3]:
+    for idx, (samples, targets) in enumerate(data_loader):
         samples = samples.cuda(non_blocking=True)
         targets = targets.cuda(non_blocking=True)
 
