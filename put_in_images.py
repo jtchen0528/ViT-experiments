572c72aff5fc14b390defa69b9376fc3dbf6b3d4
Jhao Ting Chen <jtchen0528@gmail.com>
2021-07-05 17:26:59 +0800

    update

--- put_in_images.py
+++ put_in_images.py
@@ -4,8 +4,8 @@ folder = 'dataset/tiny-imagenet-200/val/'
 
 classlist = os.listdir(folder)
 
-for class in classlist:
-    imagelist = os.listdir(folder + class)
-    os.makedirs(folder + class + '/images')
+for c in classlist:
+    imagelist = os.listdir(folder + c)
+    os.makedirs(folder + c + '/images')
     for image in imagelist:
-        os.rename(folder + class + '/' + image, folder + class + '/images/' + image)
+        os.rename(folder + c + '/' + image, folder + c + '/images/' + image)
