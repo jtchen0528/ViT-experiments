0b122ecbdd921eda3a2122bac7841a82b2061e73
Jhao Ting Chen <jtchen0528@gmail.com>
2021-07-06 10:16:50 +0800

    udpate

--- train.py
+++ train.py
@@ -261,6 +261,9 @@ elif MODE == 'EVAL':
             print('top1 accuracy: %.2f' % accuracy_top1, '| top5 accuracy: %.2f' % accuracy_top5, end="\r", flush=True)
         except:
             pass
+    print(sum(acc_top1_list))
+    print(len(acc_top1_list))
+    print(sum(acc_top1_list) / len(acc_top1_list))
     print('Total top1 accuracy: %.2f' % sum(acc_top1_list) / len(acc_top1_list), '| Total top5 accuracy: %.2f' % sum(acc_top5_list) / len(acc_top5_list))
 
 # %%
